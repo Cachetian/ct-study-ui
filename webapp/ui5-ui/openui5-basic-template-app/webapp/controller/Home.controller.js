0bcbd8a4ca1ef5c454e27556fb7092211fa9dcd6
Sky <1989mutian@163.com>
2019-01-28 23:47:59 +0800

    Update Home.controller.js
    Add message view code.

--- webapp/ui5-ui/openui5-basic-template-app/webapp/controller/Home.controller.js
+++ webapp/ui5-ui/openui5-basic-template-app/webapp/controller/Home.controller.js
@@ -11,6 +11,105 @@ sap.ui.define([
 
     onInit: function() {
       sap.ui.getCore().getEventBus().subscribe("chanel1", "event1", this.onEvent1, this);
+      var that = this;
+
+      var oLink = new sap.m.Link({
+        text: "Show more information",
+        href: "http://sap.com",
+        target: "_blank"
+      });
+
+      var oMessageTemplate = new sap.m.MessageItem({
+        type: '{type}',
+        title: '{title}',
+        description: '{description}',
+        subtitle: '{subtitle}',
+        counter: '{counter}',
+        markupDescription: '{markupDescription}',
+        link: oLink
+      });
+
+      var aMockMessages = [{
+        type: 'Error',
+        title: 'Error message',
+        description: 'First Error message description. \n' +
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+        subtitle: 'Example of subtitle',
+        counter: 1
+      }, {
+        type: 'Warning',
+        title: 'Warning without description',
+        description: ''
+      }, {
+        type: 'Success',
+        title: 'Success message',
+        description: 'First Success message description',
+        subtitle: 'Example of subtitle',
+        counter: 1
+      }, {
+        type: 'Error',
+        title: 'Error message',
+        description: 'Second Error message description',
+        subtitle: 'Example of subtitle',
+        counter: 2
+      }, {
+        type: 'Information',
+        title: 'Information message',
+        description: 'First Information message description',
+        subtitle: 'Example of subtitle',
+        counter: 1
+      }];
+
+      var oModel = new sap.ui.model.json.JSONModel();
+
+      oModel.setData(aMockMessages);
+
+      this.oMessageView = new sap.m.MessageView({
+        showDetailsPageHeader: false,
+        itemSelect: function() {
+          oBackButton.setVisible(true);
+        },
+        items: {
+          path: "/",
+          template: oMessageTemplate
+        }
+      });
+
+      var oBackButton = new sap.m.Button({
+        icon: sap.ui.core.IconPool.getIconURI("nav-back"),
+        visible: false,
+        press: function() {
+          that.oMessageView.navigateBack();
+          this.setVisible(false);
+        }
+      });
+
+
+
+      this.oMessageView.setModel(oModel);
+
+      this.oDialog = new sap.m.Dialog({
+        resizable: true,
+        content: this.oMessageView,
+        state: 'Error',
+        beginButton: new sap.m.Button({
+          press: function() {
+            this.getParent().close();
+          },
+          text: "Close"
+        }),
+        customHeader: new sap.m.Bar({
+          contentMiddle: [
+            new sap.m.Text({
+              text: "Error"
+            })
+          ],
+          contentLeft: [oBackButton]
+        }),
+        contentHeight: "300px",
+        contentWidth: "500px",
+        verticalScrolling: false
+      });
     },
 
     onConfirm: function() {
@@ -29,6 +128,10 @@ sap.ui.define([
 
     onEvent1: function(param) {
       sap.m.MessageToast.show("param:" + param);
+    },
+
+    onMessageDialog: function() {
+      this.oDialog.open();
     },
 
     //
